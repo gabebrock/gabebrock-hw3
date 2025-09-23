7575b4cbd7d7a0e151b1ee027845420562a61b03
“gabebrock” <gabebrock04@gmail.com>
2025-09-22 21:34:08 -0400

    refactor: remove conditional rendering of ConnectSupabaseSteps component

--- cs1060_hw3/app/page.tsx
+++ cs1060_hw3/app/page.tsx
@@ -28,7 +28,7 @@ export default function Home() {
         <div className="flex-1 flex flex-col gap-20 max-w-5xl p-5">
           <Hero />
           <main className="flex-1 flex flex-col gap-6 px-4">
-            {hasEnvVars ? <UserTypeSelection /> : <ConnectSupabaseSteps />}
+            <UserTypeSelection />
           </main>
         </div>
 
